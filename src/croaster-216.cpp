62f3a58a035780550afaf5876d69108557c3c5bb
IiemB <iiem.ahmad@ukur.com>
2022-06-04 21:41:05 +0700

    INIT

--- src/croaster-216.cpp
+++ src/croaster-216.cpp
@@ -1,18 +1,9 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include <TimeLib.h>
 #include <WebSocketsServer.h>
 #include <WiFiManager.h>
-#include <Thermocouple.h>
-#include <MAX6675_Thermocouple.h>
-#include <SPI.h>
 #include "DHT.h"
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <ezButton.h>
 
 #define debug(x) Serial.print(x)
 #define debugln(x) Serial.println(x)
@@ -22,14 +13,6 @@
 String inString = String(maxLenght);
 bool isArtisan = true;
 
-//  Setup MAX6675
-#define SCK_PIN D8
-#define SO_PIN D7
-#define CS_PIN_BT D5
-#define CS_PIN_ET D6
-
-Thermocouple *thermocouple_bt;
-Thermocouple *thermocouple_et;
 float timer = 0;
 int temp_et = 0;
 int temp_bt = 0;
@@ -38,10 +21,8 @@ float ror_bt = 0;
 int arrEt[61] = {};
 int arrBt[61] = {};
 float arrTimer[61] = {};
-bool isEtBtSwapped = false;
 bool isArrInitialized = false;
 
-//  Setup DHT11
 #define DHT_PIN D4
 #define DHTTYPE DHT11
 DHT dht(DHT_PIN, DHTTYPE);
@@ -64,42 +45,7 @@ WiFiManager wifiManager;
 // Setup WebSocket
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-//  Setup time and date
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "0.id.pool.ntp.org", 25200, 60000);
-char Time[] = "TIME:00:00:00";
-char Date[] = "DATE:00/00/2000";
-byte last_second, second_, minute_, hour_, day_, month_;
-int year_;
-
-// Setup button
-ezButton button(D3);
-
-// Setup LCD
-LiquidCrystal_I2C display(0x27, 16, 2);
-uint8_t selectedAdditonalDisplay;
-
-// Setup Croaster
-float fwVersion = 2.0;
-
-void splash()
-{
-  for (int i = 0; i <= 7; i++)
-  {
-
-    display.clear();
-    display.setCursor(3, 0);
-    display.print("WELCOME TO");
-    display.setCursor(4, 1);
-    display.print("CROASTER");
-    display.display();
-    delay(250);
-    display.clear();
-    delay(250);
-  }
-
-  delay(3000);
-}
+float fwVersion = 0.99;
 
 void getRor(int et, int bt, float timer)
 {
@@ -156,7 +102,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
   {
     debugln("# Websocket is connected");
     debugln("# " + webSocket.remoteIP(num).toString());
-    webSocket.sendTXT(num, "connected");
+    // webSocket.sendTXT(num, "connected");
   }
   break;
   case WStype_TEXT:
@@ -204,17 +150,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
       intervalSendData = 5000;
     }
 
-    if (cmd == "swapetbt")
-    {
-      isEtBtSwapped = true;
-    }
-
-    if (cmd == "swapbtet")
-    {
-      isEtBtSwapped = false;
-    }
-
-    webSocket.sendTXT(num, cmd);
+    // webSocket.sendTXT(num, cmd);
     // send response to mobile, if command is "poweron" then response will be "poweron:success"
     // this response can be used to track down the success of command in mobile app.
     break;
@@ -250,125 +186,28 @@ void handleArtisan()
   }
 }
 
-void timeUpdater()
-{
-  timeClient.update();
-  unsigned long unix_epoch = timeClient.getEpochTime();
-
-  second_ = second(unix_epoch);
-  if (last_second != second_)
-  {
-    minute_ = minute(unix_epoch);
-    hour_ = hour(unix_epoch);
-    day_ = day(unix_epoch);
-    month_ = month(unix_epoch);
-    year_ = year(unix_epoch);
-
-    Time[12] = second_ % 10 + 48;
-    Time[11] = second_ / 10 + 48;
-    Time[9] = minute_ % 10 + 48;
-    Time[8] = minute_ / 10 + 48;
-    Time[6] = hour_ % 10 + 48;
-    Time[5] = hour_ / 10 + 48;
-
-    Date[5] = day_ / 10 + 48;
-    Date[6] = day_ % 10 + 48;
-    Date[8] = month_ / 10 + 48;
-    Date[9] = month_ % 10 + 48;
-    Date[13] = (year_ / 10) % 10 + 48;
-    Date[14] = year_ % 10 % 10 + 48;
-
-    last_second = second_;
-  }
-}
-
-void updateDisplay()
-{
-  display.clear();
-  switch (selectedAdditonalDisplay)
-  {
-  case 0:
-    display.setCursor(0, 0);
-    display.print("Drum Temp: " + String(temp_et) + " C");
-    display.setCursor(0, 1);
-    display.print("Bean Temp: " + String(temp_bt) + " C");
-    break;
-  case 1:
-    display.setCursor(0, 0);
-    display.print(Time);
-    display.setCursor(0, 1);
-    display.print(Date);
-    break;
-  case 2:
-    display.setCursor(0, 0);
-    display.print("Croaster IP Addr");
-    display.setCursor(0, 1);
-    display.print(WiFi.localIP().toString());
-    break;
-  case 3:
-    display.setCursor(0, 0);
-    display.print("R Temp: " + String(temp) + " C");
-    display.setCursor(0, 1);
-    display.print("R Humd: " + String(humd) + " %");
-    break;
-  default:
-    break;
-  }
-  display.display();
-}
-
 void setup()
 {
   Serial.begin(115200);
-  button.setDebounceTime(50);
 
-  display.init();
-  display.backlight();
-
-  splash();
-
-  thermocouple_bt = new MAX6675_Thermocouple(SCK_PIN, CS_PIN_BT, SO_PIN);
-  thermocouple_et = new MAX6675_Thermocouple(SCK_PIN, CS_PIN_ET, SO_PIN);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 
   wifiManager.setConfigPortalBlocking(false);
   wifiManager.setAPCallback(configModeCallback);
 
-  display.clear();
-  display.setCursor(0, 0);
-  display.print("Firmware Version");
-  display.setCursor(0, 1);
-  display.print(String(fwVersion));
-  display.display();
-  delay(2000);
-
-  if (wifiManager.autoConnect("Croaster"))
+  if (wifiManager.autoConnect("Craoster Dummy Data"))
   {
     String connectedSSID = wifiManager.getWiFiSSID(true);
     debugln("# WiFi Connected... yeey :)");
-
-    display.clear();
-    display.setCursor(0, 0);
-    display.print("WiFi Connected");
-    display.setCursor(0, 1);
-    display.print(connectedSSID);
-    display.display();
     delay(2000);
   }
   else
   {
     debugln("# Configportal running");
-
-    display.clear();
-    display.setCursor(4, 0);
-    display.print("WiFi Not");
-    display.setCursor(4, 1);
-    display.print("Connected");
-    display.display();
     delay(2000);
   }
 
-  dht.begin();
-  timeClient.begin();
   webSocket.begin();                 // websocket Begin
   webSocket.onEvent(webSocketEvent); // set Event for websocket
   debugln("# Websocket is started");
@@ -376,25 +215,20 @@ void setup()
 
 void loop()
 {
-  button.loop();
   wifiManager.process();
   webSocket.loop();
   handleArtisan();
-  timeUpdater();
 
   if (millis() - millisReadTemp >= 250)
   {
+    if (isWifiConnected)
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+
     millisReadTemp = millis();
-    if (isEtBtSwapped)
-    {
-      temp_bt = thermocouple_et->readCelsius();
-      temp_et = thermocouple_bt->readCelsius();
-    }
-    else
-    {
-      temp_et = thermocouple_et->readCelsius();
-      temp_bt = thermocouple_bt->readCelsius();
-    }
+    temp_bt = random(25, 71);
+    temp_et = random(25, 71);
 
     timer = millis() * 0.001;
 
@@ -410,8 +244,8 @@ void loop()
       temp_et = 0;
     }
 
-    humd = dht.readHumidity();
-    temp = dht.readTemperature();
+    humd = random(19, 50);
+    temp = random(19, 50);
 
     if (isnan(humd) || isnan(temp))
     {
@@ -429,25 +263,26 @@ void loop()
 
   if (millis() - millisUpdateDisplay >= 1500)
   {
+    if (isWifiConnected)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+
     millisUpdateDisplay = millis();
 
     getRor(temp_et, temp_bt, timer);
-
-    updateDisplay();
   }
 
   if (millis() - millisWebSocket >= intervalSendData)
   {
+    if (isWifiConnected)
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+
     millisWebSocket = millis();
 
-    if (WiFi.status() == WL_CONNECTED)
-    {
-      isWifiConnected = true;
-    }
-    else
-    {
-      isWifiConnected = false;
-    }
+    isWifiConnected = WiFi.status() == WL_CONNECTED;    
 
     String arrJsonData[11] = {
         "{",
@@ -472,14 +307,7 @@ void loop()
 
     webSocket.broadcastTXT(jsonData);
     debugln("# isWifiConnected: " + String(isWifiConnected));
-    debugln("# isEtBtSwapped: " + String(isEtBtSwapped));
     debugln("# intervalSendData: " + String(intervalSendData));
     debugln("# Json Data: " + jsonData);
-  }
-
-  if (button.isPressed())
-  {
-    if (++selectedAdditonalDisplay == 4)
-      selectedAdditonalDisplay = 0;
-  }
+  } 
 }
