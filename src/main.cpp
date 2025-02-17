898e8f23af2d91099b736b3cb647059d663baae6
Iiemb <iiem.ahmad@ukur.com>
2025-02-17 11:30:47 +0700

    remove README files and add Croaster class with sensor integration and JSON data handling

--- src/main.cpp
+++ src/main.cpp
@@ -29,184 +29,184 @@ void configModeCallback(WiFiManager *myWiFiManager);
 // Function Implementations
 void setup()
 {
-  Serial.begin(115200);
-  display.init();
-  display.backlight();
-  croaster.init();
+    Serial.begin(115200);
+    display.init();
+    display.backlight();
+    croaster.init();
 
-  wifiManager.setDebugOutput(croaster.useDummyData);
-  wifiManager.setConfigPortalBlocking(false);
-  wifiManager.setAPCallback(configModeCallback);
+    wifiManager.setDebugOutput(croaster.useDummyData);
+    wifiManager.setConfigPortalBlocking(false);
+    wifiManager.setAPCallback(configModeCallback);
 
-  wifiManager.setClass("invert");
+    wifiManager.setClass("invert");
 
-  // set custom ip for portal
-  wifiManager.setAPStaticIPConfig(IPAddress(10, 0, 1, 1), IPAddress(10, 0, 1, 1), IPAddress(255, 255, 255, 0));
+    // set custom ip for portal
+    wifiManager.setAPStaticIPConfig(IPAddress(10, 0, 1, 1), IPAddress(10, 0, 1, 1), IPAddress(255, 255, 255, 0));
 
-  if (wifiManager.autoConnect(croaster.ssidName.c_str()))
-    debugln("# WiFi Connected");
+    if (wifiManager.autoConnect(croaster.ssidName.c_str()))
+        debugln("# WiFi Connected");
 
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
-  debugln("# WebSocket started");
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+    debugln("# WebSocket started");
 }
 
 void loop()
 {
-  wifiManager.process();
-  webSocket.loop();
-  croaster.loop();
-  updateDisplay();
-  broadcastData();
+    wifiManager.process();
+    webSocket.loop();
+    croaster.loop();
+    updateDisplay();
+    broadcastData();
 }
 
 void updateDisplay()
 {
-  if (millis() - lastDisplayUpdate < 1500)
-    return;
-  lastDisplayUpdate = millis();
+    if (millis() - lastDisplayUpdate < 1500)
+        return;
+    lastDisplayUpdate = millis();
 
-  display.clear();
-  display.setCursor(0, 0);
-  display.print("ET: " + String(croaster.tempET) + " BT: " + String(croaster.tempBT));
-  display.setCursor(0, 1);
-  if (showIp)
-  {
-    display.print(WiFi.localIP().toString());
-    showIp = false;
-  }
-  else
-  {
-    display.print("T: " + String((int)croaster.tempR) + String(croaster.temperatureUnit) + "H: " + String((int)croaster.humidity) + "%");
-    showIp = true;
-  }
+    display.clear();
+    display.setCursor(0, 0);
+    display.print("ET: " + String(croaster.tempET) + " BT: " + String(croaster.tempBT));
+    display.setCursor(0, 1);
+    if (showIp)
+    {
+        display.print(WiFi.localIP().toString());
+        showIp = false;
+    }
+    else
+    {
+        display.print("T: " + String((int)croaster.tempR) + String(croaster.temperatureUnit) + "H: " + String((int)croaster.humidity) + "%");
+        showIp = true;
+    }
 }
 
 void broadcastData()
 {
-  if (millis() - lastWebSocketSend < croaster.intervalSendData)
-    return;
-  lastWebSocketSend = millis();
+    if (millis() - lastWebSocketSend < croaster.intervalSendData)
+        return;
+    lastWebSocketSend = millis();
 
-  String jsonData = croaster.getJsonData(socketEventMessage);
+    String jsonData = croaster.getJsonData(socketEventMessage);
 
-  webSocket.broadcastTXT(jsonData);
+    webSocket.broadcastTXT(jsonData);
 
-  debugln("");
-  debugln("# " + WiFi.localIP().toString());
-  debugln("# Json Data: " + jsonData);
+    debugln("");
+    debugln("# " + WiFi.localIP().toString());
+    debugln("# Json Data: " + jsonData);
 
-  socketEventMessage = "";
+    socketEventMessage = "";
 }
 
 void handleWebSocketEvent(const String &cmd, uint8_t num)
 {
-  StaticJsonDocument<96> request;
-  if (deserializeJson(request, cmd))
-  {
-    debugln("# Invalid JSON command");
-    return;
-  }
-
-  if (request["command"].is<String>())
-  {
-    String command = request["command"];
-
-    socketEventMessage = command;
-
-    // Send data to Artisan
-    if (command == "getData")
+    StaticJsonDocument<96> request;
+    if (deserializeJson(request, cmd))
     {
-      croaster.idJsonData = request["id"];
-
-      socketEventMessage = "";
-
-      String jsonData = croaster.getJsonData(socketEventMessage, true);
-
-      webSocket.broadcastTXT(jsonData);
-
-      debugln("");
-      debugln("# Json Data Artisan: " + jsonData);
-
-      return;
+        debugln("# Invalid JSON command");
+        return;
     }
 
-    if (command == "restartesp")
+    if (request["command"].is<String>())
     {
-      String jsonData = croaster.getJsonData(socketEventMessage);
+        String command = request["command"];
 
-      webSocket.sendTXT(num, jsonData);
+        socketEventMessage = command;
 
-      ESP.restart();
+        // Send data to Artisan
+        if (command == "getData")
+        {
+            croaster.idJsonData = request["id"];
 
-      return;
+            socketEventMessage = "";
+
+            String jsonData = croaster.getJsonData(socketEventMessage, true);
+
+            webSocket.broadcastTXT(jsonData);
+
+            debugln("");
+            debugln("# Json Data Artisan: " + jsonData);
+
+            return;
+        }
+
+        if (command == "restartesp")
+        {
+            String jsonData = croaster.getJsonData(socketEventMessage);
+
+            webSocket.sendTXT(num, jsonData);
+
+            ESP.restart();
+
+            return;
+        }
+
+        if (command == "erase")
+        {
+            String jsonData = croaster.getJsonData(socketEventMessage);
+
+            webSocket.sendTXT(num, jsonData);
+
+            wifiManager.erase();
+
+            ESP.restart();
+
+            return;
+        }
     }
 
-    if (command == "erase")
+    if (request["command"].is<JsonObject>())
     {
-      String jsonData = croaster.getJsonData(socketEventMessage);
+        JsonObject command = request["command"];
 
-      webSocket.sendTXT(num, jsonData);
+        // Handle interval update
+        if (command.containsKey("interval") && command["interval"].is<int>())
+        {
+            int intervalSeconds = command["interval"]; // Read interval value
 
-      wifiManager.erase();
+            croaster.intervalSendData = intervalSeconds * 1000; // Convert seconds to milliseconds
 
-      ESP.restart();
+            debugln("# Interval updated to " + String(intervalSeconds) + " seconds");
 
-      return;
+            socketEventMessage = String(intervalSeconds) + "seconds";
+        }
+
+        if (command.containsKey("tempUnit") && command["tempUnit"].is<String>())
+        {
+            String tempUnit = command["tempUnit"]; // Read tempUnit value
+
+            croaster.changeTemperatureUnit(tempUnit);
+
+            debugln("# Temperature Unit updated to " + tempUnit);
+
+            socketEventMessage = tempUnit;
+        }
     }
-  }
-
-  if (request["command"].is<JsonObject>())
-  {
-    JsonObject command = request["command"];
-
-    // Handle interval update
-    if (command.containsKey("interval") && command["interval"].is<int>())
-    {
-      int intervalSeconds = command["interval"]; // Read interval value
-
-      croaster.intervalSendData = intervalSeconds * 1000; // Convert seconds to milliseconds
-
-      debugln("# Interval updated to " + String(intervalSeconds) + " seconds");
-
-      socketEventMessage = String(intervalSeconds) + "seconds";
-    }
-
-    if (command.containsKey("tempUnit") && command["tempUnit"].is<String>())
-    {
-      String tempUnit = command["tempUnit"]; // Read tempUnit value
-
-      croaster.changeTemperatureUnit(tempUnit);
-
-      debugln("# Temperature Unit updated to " + tempUnit);
-
-      socketEventMessage = tempUnit;
-    }
-  }
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 {
-  switch (type)
-  {
-  case WStype_DISCONNECTED:
-    debugln("# WebSocket disconnected");
-    break;
+    switch (type)
+    {
+    case WStype_DISCONNECTED:
+        debugln("# WebSocket disconnected");
+        break;
 
-  case WStype_CONNECTED:
-    debugln("# WebSocket connected");
-    break;
+    case WStype_CONNECTED:
+        debugln("# WebSocket connected");
+        break;
 
-  case WStype_TEXT:
-    handleWebSocketEvent(String((char *)payload), num);
-    break;
+    case WStype_TEXT:
+        handleWebSocketEvent(String((char *)payload), num);
+        break;
 
-  default:
-    break;
-  }
+    default:
+        break;
+    }
 }
 
 void configModeCallback(WiFiManager *myWiFiManager)
 {
-  debugln("# Config mode: " + WiFi.softAPIP().toString());
+    debugln("# Config mode: " + WiFi.softAPIP().toString());
 }
